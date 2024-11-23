b3ec77ccee68e950eda9ba10a50950e8e944157f
Jehee-H <75777964+Jehee-H@users.noreply.github.com>
2024-11-23 21:02:03 +0100

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const images = ['../assets/image1.jpg', '../assets/image2.jpg', '../assets/image3.jpg', '../assets/image4.jpg'];
     let currentImageIndex = 0;
 
+    //asdasd
+
     const imgSlider = document.querySelector('.img-slider');
     const dots = document.querySelectorAll('.dot');
     const prevArrow = document.querySelector('.prev');
